24f13a73796144d966e7ef13081157743228e9d0
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-05-11 21:49:37 -0400

    Questão 2 finalizada

--- QUESTAO 2/mostraLoteria.php
+++ QUESTAO 2/mostraLoteria.php
@@ -5,6 +5,11 @@
  * a cor azul(#0000FF) os números sorteados. 
  * Para isso, deverão constar 3 apostas não repetidas
  */
+
+include("../QUESTAO 1/loteria.php");
+$apostaUm = loteria();
+$apostaDois = loteria();
+$apostaTres = loteria();
 ?>
 
 
@@ -13,199 +18,227 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado Mega-Quina</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>  
+    <title>Resultado Mega-Sena</title>
 </head>
 <body>
-    <h1>Aposta 1</h1>
-    <table>
-        <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-            <td>10</td>
-        </tr>
-        <tr>
-            <td>11</td>
-            <td>12</td>
-            <td>13</td>
-            <td>14</td>
-            <td>15</td>
-            <td>16</td>
-            <td>17</td>
-            <td>18</td>
-            <td>19</td>
-            <td>20</td>
-        </tr>
-        <tr>
-            <td>21</td>
-            <td>22</td>
-            <td>23</td>
-            <td>24</td>
-            <td>25</td>
-            <td>26</td>
-            <td>27</td>
-            <td>28</td>
-            <td>29</td>
-            <td>30</td>
-        </tr>
-        <tr>
-            <td>31</td>
-            <td>32</td>
-            <td>33</td>
-            <td>34</td>
-            <td>35</td>
-            <td>36</td>
-            <td>37</td>
-            <td>38</td>
-            <td>39</td>
-            <td>40</td>
-        </tr>
-        <tr>
-            <td>41</td>
-            <td>42</td>
-            <td>43</td>
-            <td>44</td>
-            <td>45</td>
-            <td>46</td>
-            <td>47</td>
-            <td>48</td>
-            <td>49</td>
-            <td>60</td>
-        </tr>
-    </table>
-
-    <h1>Aposta 2</h1>
-    <table>
-        <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-            <td>10</td>
-        </tr>
-        <tr>
-            <td>11</td>
-            <td>12</td>
-            <td>13</td>
-            <td>14</td>
-            <td>15</td>
-            <td>16</td>
-            <td>17</td>
-            <td>18</td>
-            <td>19</td>
-            <td>20</td>
-        </tr>
-        <tr>
-            <td>21</td>
-            <td>22</td>
-            <td>23</td>
-            <td>24</td>
-            <td>25</td>
-            <td>26</td>
-            <td>27</td>
-            <td>28</td>
-            <td>29</td>
-            <td>30</td>
-        </tr>
-        <tr>
-            <td>31</td>
-            <td>32</td>
-            <td>33</td>
-            <td>34</td>
-            <td>35</td>
-            <td>36</td>
-            <td>37</td>
-            <td>38</td>
-            <td>39</td>
-            <td>40</td>
-        </tr>
-        <tr>
-            <td>41</td>
-            <td>42</td>
-            <td>43</td>
-            <td>44</td>
-            <td>45</td>
-            <td>46</td>
-            <td>47</td>
-            <td>48</td>
-            <td>49</td>
-            <td>60</td>
-        </tr>
-    </table>
-
-    <h1>Aposta 3</h1>
-    <table>
-        <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-            <td>10</td>
-        </tr>
-        <tr>
-            <td>11</td>
-            <td>12</td>
-            <td>13</td>
-            <td>14</td>
-            <td>15</td>
-            <td>16</td>
-            <td>17</td>
-            <td>18</td>
-            <td>19</td>
-            <td>20</td>
-        </tr>
-        <tr>
-            <td>21</td>
-            <td>22</td>
-            <td>23</td>
-            <td>24</td>
-            <td>25</td>
-            <td>26</td>
-            <td>27</td>
-            <td>28</td>
-            <td>29</td>
-            <td>30</td>
-        </tr>
-        <tr>
-            <td>31</td>
-            <td>32</td>
-            <td>33</td>
-            <td>34</td>
-            <td>35</td>
-            <td>36</td>
-            <td>37</td>
-            <td>38</td>
-            <td>39</td>
-            <td>40</td>
-        </tr>
-        <tr>
-            <td>41</td>
-            <td>42</td>
-            <td>43</td>
-            <td>44</td>
-            <td>45</td>
-            <td>46</td>
-            <td>47</td>
-            <td>48</td>
-            <td>49</td>
-            <td>60</td>
-        </tr>
-    </table>
+    <div class="row col-md-12">
+        <div class="column col-md-4">
+            <h1>Aposta 1</h1>
+            <table>
+                <tr>
+                    <?php 
+                        
+                        for($i = 1; $i <= 10; $i++) {
+                            if(in_array($i, $apostaUm)){                        
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>            
+                </tr>
+                <tr>
+                    <?php 
+                        for($i = 11; $i <= 20; $i++) {
+                            if(in_array($i, $apostaUm)){
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>
+                </tr>
+                <tr>
+                    <?php 
+                        for($i = 21; $i <= 30; $i++) {
+                            if(in_array($i, $apostaUm)){
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>
+                </tr>
+                <tr>
+                    <?php 
+                        for($i = 31; $i <= 40; $i++) {
+                            if(in_array($i, $apostaUm)){
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>
+                </tr>
+                <tr>
+                    <?php 
+                        for($i = 41; $i <= 50; $i++) {
+                            if(in_array($i, $apostaUm)){
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>
+                </tr>
+            </table>
+        </div>
+        <div class="column col-md-4">
+            <h1>Aposta 2</h1>
+            <table>
+                <tr>
+                    <?php 
+                        for($i = 1; $i <= 10; $i++) {
+                            if(in_array($i, $apostaDois)){
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>            
+                </tr>
+                <tr>
+                    <?php 
+                        for($i = 11; $i <= 20; $i++) {
+                            if(in_array($i, $apostaDois)){
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>
+                </tr>
+                <tr>
+                    <?php 
+                        for($i = 21; $i <= 30; $i++) {
+                            if(in_array($i, $apostaDois)){
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>
+                </tr>
+                <tr>
+                    <?php 
+                        for($i = 31; $i <= 40; $i++) {
+                            if(in_array($i, $apostaDois)){
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>
+                </tr>
+                <tr>
+                    <?php 
+                        for($i = 41; $i <= 50; $i++) {
+                            if(in_array($i, $apostaDois)){
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>
+                </tr>
+            </table>
+        </div>
+        <div class="column col-md-4">
+            <h1>Aposta 3</h1>
+            <table>
+                <tr>
+                    <?php 
+                        for($i = 1; $i <= 10; $i++) {
+                            if(in_array($i, $apostaTres)){
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>            
+                </tr>
+                <tr>
+                    <?php 
+                        for($i = 11; $i <= 20; $i++) {
+                            if(in_array($i, $apostaTres)){
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>
+                </tr>
+                <tr>
+                    <?php 
+                        for($i = 21; $i <= 30; $i++) {
+                            if(in_array($i, $apostaTres)){
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>
+                </tr>
+                <tr>
+                    <?php 
+                        for($i = 31; $i <= 40; $i++) {
+                            if(in_array($i, $apostaTres)){
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>
+                </tr>
+                <tr>
+                    <?php 
+                        for($i = 41; $i <= 50; $i++) {
+                            if(in_array($i, $apostaTres)){
+                                echo("<td style='background-color: #0000FF;'>$i</td>");
+                            }
+                            else
+                            {
+                                echo("<td>$i</td>");
+                            }
+                        }
+                    ?>
+                </tr>
+            </table>
+        </div>
+    </div>
 </body>
 </html>
