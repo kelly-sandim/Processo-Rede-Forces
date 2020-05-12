f53df5af6c73715e2b826888ad34e9be02b8e4c8
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-05-12 10:24:13 -0400

    Criado os arquivos base da questão 4

---
+++ QUESTAO 4/uploadTexto.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+ * Utilizando a classe criada acima, criar um form 
+ * que solicite ao usuário um arquivo e que ao 
+ * submeter seja visualizado o conteúdo
+ */
+
+include("../QUESTAO 3/lerTexto.php");
+$leitor = new leitorTexto();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>  
+    <title>Leitor de Arquivos</title>
+</head>
+<body>
+    <h1>Faça upload de um arquivo e veja o resultado abaixo!</h1>
+    
+</body>
+</html>
